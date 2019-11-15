ccad29dae41f4b0af98ebd0c503d9534ad3e25d3
Indrawan2727 <56685191+Indrawan2727@users.noreply.github.com>
2019-11-15 14:40:14 +0700

    Add files via upload

---
+++ Pertemuan5/Server.py
@@ -0,0 +1,30 @@
+import socket
+
+def server_program():
+    #get the hostname
+    host = socket.gethostname()
+    port = 5000 # initiate port no above 1024
+
+    server_socket = socket.socket() #get instance
+    #look closely. The  bind () function takes tuple as argument
+    server_socket.bind((host, port)) # bind host address and port
+
+    #configure how many client the server can listen simultaneously
+    server_socket.listen(2)
+    conn, address = server_socket.accept()
+    print("Connection from: " + str(address))
+    while True:
+        # receivedata stream. it won't accept data packet greater
+        data = conn.recv(1024).decode()
+        if not data:
+            # if data is not received break
+            break
+        print("from connected user: " + str(data))
+        data = input('->')
+        conn.send(data.encode()) #send data to the client
+
+    conn.close()# close the connection
+
+server_program()
+#if__name__== '__name__':
+#server_program()
